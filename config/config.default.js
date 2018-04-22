cb470b2e9b3cf2d9daa360027f5d5f1af1b20fed
Zixian Song <zs@lendify.se>
2018-04-22 18:03:11 +0200

    init 2

---
+++ config/config.default.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1524407311637_9177';
+
+  exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
